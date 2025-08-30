cd697a9ee07e48ace6050acd95943368ae3d2178
DrHongXu <80917018+DrHongXu@users.noreply.github.com>
2025-08-31 01:21:00 +0200

    Add files via upload

--- update_info_script.js
+++ update_info_script.js
@@ -1,24 +1,26 @@
 // updateInfo.js
 
 const data = {
-    "web_update_year": 2024,
-    "web_update_month": 11,  // 两位数，01-09
+    "age": new Date().getFullYear() - 1993 ,
+    "web_update_year": 2025,
+    "web_update_month": "08",  // 两位数，01-09
     "web_update_day": 1,
     "born_year": 1993,
-    "age": 31,
     "year_of_experience": 8,
     "no_of_papers": 15,
-    "google_scholar_citation": 330,
-    "h_index": 9,
+    "google_scholar_citation": 430,
+    "h_index": 10,
     "no_of_reviews": 55,
     "no_of_patents": 3,
+    "no_of_patents_in_review": 2,
+    "no_of_patents_sum": 5,
     "patent_citation": 18,
     "no_of_books": 0,
     "phd_thesis_views": 2200,
     "zhihu_followers": 13800,
     "linkedin_followers": 1900,
     "xiaohongshu_followers": 3200,
-    "visited_countries": 41
+    "visited_countries": 40
 };
 
 // 更新指定类名的 <span> 内容的函数
