2b7708b9b3a95bc86311dd4ab9ec3cdfea7e6298
DrHongXu <80917018+DrHongXu@users.noreply.github.com>
2025-09-17 21:50:48 +0200

    up

--- update_info_script.js
+++ update_info_script.js
@@ -1,15 +1,17 @@
 // updateInfo.js
 
+// 固定的个人信息，包含默认的 citation
 const data = {
-    "age": new Date().getFullYear() - 1993 ,
+    "age": new Date().getFullYear() - 1993,
     "web_update_year": 2025,
-    "web_update_month": "09",  // 两位数，01-09
+    "web_update_month": "09",
     "web_update_day": 1,
     "born_year": 1993,
     "year_of_experience": 8,
     "no_of_papers": 15,
-    "google_scholar_citation": 430,
+    "google_scholar_citation": 440,  // 默认值
     "h_index": 10,
+    "no_of_journals_for_review": 10,
     "no_of_reviews": 55,
     "no_of_patents": 3,
     "no_of_patents_in_review": 2,
@@ -17,11 +19,14 @@ const data = {
     "patent_citation": 18,
     "no_of_books": 0,
     "phd_thesis_views": 2200,
+    "phd_thesis_downloads": 660,
     "zhihu_followers": 13800,
     "linkedin_followers": 1900,
     "xiaohongshu_followers": 3200,
     "visited_countries": 40
-};
+  };
+  
+  
 
 // 更新指定类名的 <span> 内容的函数
 function updateContent() {
